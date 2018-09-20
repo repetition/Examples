bf663a6041b0ee880b0a840bbbb62c9d61cd5a99
1115746521@qq.com <zhangxi111574>
2018-09-20 23:01:51 +0800

    解析公告列表

--- src/main/java/WebMagic/WebMagicMain.java
+++ src/main/java/WebMagic/WebMagicMain.java
@@ -1,6 +1,5 @@
 package WebMagic;
 
-import com.sun.mail.imap.protocol.ID;
 import org.hyperic.sigar.RPC;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,6 +21,8 @@ import java.net.SocketException;
 import java.nio.charset.Charset;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * http://www.caam.org.cn/newslist/a95-1.html  公告信息
@@ -34,116 +35,10 @@ public class WebMagicMain {
     public static void main(String[] args) {
 
         // Spider.create(new VehicleAnnouncementPageProcessor()).addUrl("http://data.miit.gov.cn/resultSearch?categoryTreeId=1128").run();
-        //  Spider.create(new NoticePageProcessor()).addUrl("http://www.caam.org.cn/newslist/a95-1.html").run();
-        log.info(getLocalMacAddressFromIp());
-        log.info(getMac());
+          Spider.create(new NoticePageProcessor()).addUrl("http://www.caam.org.cn/newslist/a95-1.html").run();
+       // log.info(getLocalMacAddressFromIp());
+        //log.info(getMac());
     }
-
-    /**
-     * 根据IP地址获取MAC地址
-     *
-     * @return
-     */
-    private static String getLocalMacAddressFromIp() {
-        String strMacAddr = null;
-        try {
-            //获得IpD地址
-            InetAddress ip = getLocalInetAddress();
-            byte[] b = NetworkInterface.getByInetAddress(ip).getHardwareAddress();
-            StringBuffer buffer = new StringBuffer();
-            for (int i = 0; i < b.length; i++) {
-                if (i != 0) {
-                    buffer.append(':');
-                }
-                String str = Integer.toHexString(b[i] & 0xFF);
-                buffer.append(str.length() == 1 ? 0 + str : str);
-            }
-            strMacAddr = buffer.toString().toUpperCase();
-        } catch (Exception e) {
-
-        }
-
-        return strMacAddr;
-    }
-
-    /**
-     * 获取移动设备本地IP
-     *
-     * @return
-     */
-    private static InetAddress getLocalInetAddress() {
-        InetAddress ip = null;
-        try {
-            //列举
-            Enumeration<NetworkInterface> en_netInterface = NetworkInterface.getNetworkInterfaces();
-            while (en_netInterface.hasMoreElements()) {//是否还有元素
-                NetworkInterface ni = (NetworkInterface) en_netInterface.nextElement();//得到下一个元素
-                Enumeration<InetAddress> en_ip = ni.getInetAddresses();//得到一个ip地址的列举
-                while (en_ip.hasMoreElements()) {
-                    ip = en_ip.nextElement();
-                    if (!ip.isLoopbackAddress() && ip.getHostAddress().indexOf(":") == -1) {
-                        break;
-                    } else {
-                        ip = null;
-                    }
-                }
-
-                if (ip != null) {
-                    break;
-                }
-            }
-        } catch (SocketException e) {
-            e.printStackTrace();
-        }
-        return ip;
-    }
-
-
-    //获取当前连接网络的网卡的mac地址
-    private static String parseByte(byte b) {
-        String s = "00" + Integer.toHexString(b)+":";
-        return s.substring(s.length() - 3);
-    }
-    /**
-     * 获取当前系统连接网络的网卡的mac地址
-     * @return
-     */
-    public static final String getMac() {
-        byte[] mac = null;
-        StringBuffer sb = new StringBuffer();
-        try {
-            Enumeration<NetworkInterface> netInterfaces = NetworkInterface.getNetworkInterfaces();
-            while (netInterfaces.hasMoreElements()) {
-                NetworkInterface ni = netInterfaces.nextElement();
-                Enumeration<InetAddress> address = ni.getInetAddresses();
-                while (address.hasMoreElements()) {
-                    InetAddress ip = address.nextElement();
-                    if (ip.isAnyLocalAddress() || !(ip instanceof Inet4Address) || ip.isLoopbackAddress())
-                        continue;
-                    if (ip.isSiteLocalAddress()) {
-                        mac = ni.getHardwareAddress();
-                    } else if (!ip.isLinkLocalAddress()) {
-                        mac = ni.getHardwareAddress();
-                        break;
-                    }
-                }
-            }
-        } catch (SocketException e) {
-            e.printStackTrace();
-        }
-
-        if(mac != null){
-            for(int i=0 ;i<mac.length ;i++){
-                sb.append(parseByte(mac[i]));
-            }
-            return sb.substring(0, sb.length()-1);
-        }else {
-            return null;
-        }
-    }
-
-
-
 
     static class NoticePageProcessor implements PageProcessor {
         private Site site = Site.me().setUserAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.26 Safari/537.36 Core/1.63.6726.400 QQBrowser/10.2.2265.400");
@@ -158,12 +53,55 @@ public class WebMagicMain {
 
             for (Selectable node : li_Nodes) {
                 Selectable a_node = node.xpath("//li//a");
+                //列表信息
                 String notice_Title = a_node.regex(">(.+?)<").get();
                 String links = a_node.links().get();
-
-                log.info(notice_Title);
-                log.info(links);
+                if (notice_Title.contains("车辆新产品公示")) {
+                    log.info(notice_Title);
+                    log.info(links);
+                    Spider.create(new NoticePageInfoProcessor()).addUrl(links).run();
+                }
             }
+        }
+
+        @Override
+        public Site getSite() {
+            site.setTimeOut(10000);
+            site.setRetryTimes(10);
+            return site;
+        }
+
+
+    }
+
+    /**
+     * 获取每个批次的公告列表
+     */
+    static class NoticePageInfoProcessor implements PageProcessor{
+        private Site site = Site.me().setUserAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.26 Safari/537.36 Core/1.63.6726.400 QQBrowser/10.2.2265.400");
+        @Override
+        public void process(Page page) {
+            Html html = page.getHtml();
+            Selectable xpath = html.xpath("//div[@class=\"newstext\"]");
+            //获取描述
+            Selectable noticePageInfo = xpath.xpath("//p");
+            String noticePageInfoStr = noticePageInfo.regex(">(.+?)<").get();
+            //获取公告列表链接
+            String links = xpath.xpath("//p//a").links().get();
+            log.info(noticePageInfoStr+"");
+            log.info(links+"");
+
+            Pattern pattern = Pattern.compile("\\（(.+?)\\）");
+            Matcher matcher = pattern.matcher(noticePageInfoStr);
+            if (matcher.find()) {
+                //获取批次
+                String batch = matcher.group(1);
+            }
+
+
+
+
+
         }
 
         @Override
@@ -278,6 +216,112 @@ public class WebMagicMain {
             e.printStackTrace();
         }
     }
+
+    /**
+     * 根据IP地址获取MAC地址
+     *
+     * @return
+     */
+    private static String getLocalMacAddressFromIp() {
+        String strMacAddr = null;
+        try {
+            //获得IpD地址
+            InetAddress ip = getLocalInetAddress();
+            byte[] b = NetworkInterface.getByInetAddress(ip).getHardwareAddress();
+            StringBuffer buffer = new StringBuffer();
+            for (int i = 0; i < b.length; i++) {
+                if (i != 0) {
+                    buffer.append(':');
+                }
+                String str = Integer.toHexString(b[i] & 0xFF);
+                buffer.append(str.length() == 1 ? 0 + str : str);
+            }
+            strMacAddr = buffer.toString().toUpperCase();
+        } catch (Exception e) {
+
+        }
+
+        return strMacAddr;
+    }
+
+    /**
+     * 获取移动设备本地IP
+     *
+     * @return
+     */
+    private static InetAddress getLocalInetAddress() {
+        InetAddress ip = null;
+        try {
+            //列举
+            Enumeration<NetworkInterface> en_netInterface = NetworkInterface.getNetworkInterfaces();
+            while (en_netInterface.hasMoreElements()) {//是否还有元素
+                NetworkInterface ni = (NetworkInterface) en_netInterface.nextElement();//得到下一个元素
+                Enumeration<InetAddress> en_ip = ni.getInetAddresses();//得到一个ip地址的列举
+                while (en_ip.hasMoreElements()) {
+                    ip = en_ip.nextElement();
+                    if (!ip.isLoopbackAddress() && ip.getHostAddress().indexOf(":") == -1) {
+                        break;
+                    } else {
+                        ip = null;
+                    }
+                }
+
+                if (ip != null) {
+                    break;
+                }
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        return ip;
+    }
+
+    //获取当前连接网络的网卡的mac地址
+    private static String parseByte(byte b) {
+        String s = "00" + Integer.toHexString(b)+":";
+        return s.substring(s.length() - 3);
+    }
+
+    /**
+     * 获取当前系统连接网络的网卡的mac地址
+     * @return
+     */
+    public static final String getMac() {
+        byte[] mac = null;
+        StringBuffer sb = new StringBuffer();
+        try {
+            Enumeration<NetworkInterface> netInterfaces = NetworkInterface.getNetworkInterfaces();
+            while (netInterfaces.hasMoreElements()) {
+                NetworkInterface ni = netInterfaces.nextElement();
+                Enumeration<InetAddress> address = ni.getInetAddresses();
+                while (address.hasMoreElements()) {
+                    InetAddress ip = address.nextElement();
+                    if (ip.isAnyLocalAddress() || !(ip instanceof Inet4Address) || ip.isLoopbackAddress())
+                        continue;
+                    if (ip.isSiteLocalAddress()) {
+                        mac = ni.getHardwareAddress();
+                    } else if (!ip.isLinkLocalAddress()) {
+                        mac = ni.getHardwareAddress();
+                        break;
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+
+        if(mac != null){
+            for(int i=0 ;i<mac.length ;i++){
+                sb.append(parseByte(mac[i]));
+            }
+            return sb.substring(0, sb.length()-1);
+        }else {
+            return null;
+        }
+    }
+
+
+
 
 
 }
